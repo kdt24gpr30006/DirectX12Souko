21bd11315e2603fbcab4447b9a199f5e5de6ab67
rento <PC_User@DESKTOP-4CJH6GI>
2025-12-23 18:03:32 +0900

    パッド入力の追加 インプットクラスをパッドとキーボードの管理に変更 testsceneにinput用imGui追加
    todo
    パッド接続
    現在dualshockは判定しないのでDS4でXBOXに変える

---
+++ ProjectDX12/ProjectDX12/FrameWork/System/Input/Pad/Pad.cpp
@@ -0,0 +1,96 @@
+﻿#include "Pad.h"
+
+using namespace winrt;
+using namespace Windows::Gaming::Input;
+
+namespace Engine::Input
+{
+    Pad::Pad(Gamepad gamepad)
+        : mGamepad(gamepad)
+    {
+    }
+
+    void Pad::Update()
+    {
+        if (!mGamepad)
+        {
+            mConnected = false;
+            return;
+        }
+
+        // 前フレーム保存
+        mPrevious = mCurrent;
+
+        // 現在の状態取得
+        mReading = mGamepad.GetCurrentReading();
+        mCurrent = mReading.Buttons;
+    }
+    
+
+    bool Pad::IsPress(GamepadButtons button) const
+    {
+        return (mCurrent & button) != GamepadButtons::None;
+    }
+
+    bool Pad::IsPush(GamepadButtons button) const
+    {
+        return ((mPrevious & button) == GamepadButtons::None) &&
+            ((mCurrent & button) != GamepadButtons::None);
+    }
+
+    bool Pad::IsRelease(GamepadButtons button) const
+    {
+        return ((mPrevious & button) != GamepadButtons::None) &&
+            ((mCurrent & button) == GamepadButtons::None);
+    }
+
+    float Pad::DeadZone(float v) const
+    {
+        // デッドゾーン内なら0を返す
+        return (std::abs(v) < mDeadZoen) ? 0.0f : v;
+    }
+
+    float Pad::LeftX() const
+    {
+        return DeadZone(static_cast<float>(mReading.LeftThumbstickX));
+    }
+
+    float Pad::LeftY() const
+    {
+        return DeadZone(static_cast<float>(mReading.LeftThumbstickY));
+    }
+
+    float Pad::RightX() const
+    {
+        return DeadZone(static_cast<float>(mReading.RightThumbstickX));
+    }
+
+    float Pad::RightY() const
+    {
+        return DeadZone(static_cast<float>(mReading.RightThumbstickY));
+    }
+
+    float Pad::LeftTrigger() const
+    {
+        return static_cast<float>(mReading.LeftTrigger);
+    }
+
+    float Pad::RightTrigger() const
+    {
+        return static_cast<float>(mReading.RightTrigger);
+    }
+
+    bool Pad::IsConnected() const
+    {
+        return mGamepad != nullptr;
+    }
+
+    Gamepad Pad::GetGamepad() const
+    {
+        return mGamepad;
+    }
+    void Pad::SetDeadZone(float argDeadZone)
+    {
+        mDeadZoen = argDeadZone;
+    }
+}
---
+++ ProjectDX12/ProjectDX12/FrameWork/System/Input/Pad/Pad.h
@@ -0,0 +1,85 @@
+﻿#pragma once
+#include <winrt/Windows.Gaming.Input.h>
+
+namespace Engine::Input
+{
+    class Pad
+    {
+    public:
+        // コンストラクタ
+        explicit Pad(winrt::Windows::Gaming::Input::Gamepad gamepad);
+
+        // 毎フレーム呼ぶ
+        void Update();
+
+        /// <summary>
+        /// ボタンが押されているか
+        /// </summary>
+        /// <param name="button">判定するボタン</param>
+        /// <returns>true : 押されている false : 押されていない</returns>
+        bool IsPress(winrt::Windows::Gaming::Input::GamepadButtons button) const;
+
+        /// <summary>
+        /// ボタンを押した瞬間か
+        /// </summary>
+        /// <param name="button">判定するボタン</param>
+        /// <returns>true : 押した瞬間 false : 押した瞬間じゃない</returns>
+        bool IsPush(winrt::Windows::Gaming::Input::GamepadButtons button) const;
+
+        /// <summary>
+        /// 離した瞬間か
+        /// </summary>
+        /// <param name="button">判定するボタン</param>
+        /// <returns>true : 離した瞬間 false : 離した瞬間じゃない</returns>
+        bool IsRelease(winrt::Windows::Gaming::Input::GamepadButtons button) const;
+
+        // スティック
+        float LeftX()  const;
+        float LeftY()  const;
+        float RightX() const;
+        float RightY() const;
+
+        // トリガー
+        float LeftTrigger()  const;
+        float RightTrigger() const;
+
+        // 状態
+        bool IsConnected() const;
+        winrt::Windows::Gaming::Input::Gamepad GetGamepad() const;
+
+        /// <summary>
+        /// デッドゾーンの設定
+        /// </summary>
+        /// <param name="argDeadZone"></param>
+        void SetDeadZone(float argDeadZone);
+
+    private:
+
+        /// <summary>
+        /// デッドゾーン
+        /// </summary>
+        /// <param name="v">スティックの移動量</param>
+        /// <returns></returns>
+        float DeadZone(float v) const;
+
+    private:
+        // パッド本体情報
+        winrt::Windows::Gaming::Input::Gamepad mGamepad = nullptr;
+
+        // パッドの入力状態
+        winrt::Windows::Gaming::Input::GamepadReading mReading{};
+
+        // 現在のボタン状態
+        winrt::Windows::Gaming::Input::GamepadButtons mCurrent =
+            winrt::Windows::Gaming::Input::GamepadButtons::None;
+
+        // 前フレームのボタン状態
+        winrt::Windows::Gaming::Input::GamepadButtons mPrevious =
+            winrt::Windows::Gaming::Input::GamepadButtons::None;
+
+        // 接続状態
+        bool mConnected = true;
+
+        float mDeadZoen = 0.2f;
+    };
+}
